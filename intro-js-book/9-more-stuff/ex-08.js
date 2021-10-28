5766c0b084d4aad8ce1dc738f9326b58df349709
cdanskey <cdanskey@gmail.com>
2021-10-28 13:51:57 -0700

    updat ex-08

--- intro-js-book/9-more-stuff/ex-08.js
+++ intro-js-book/9-more-stuff/ex-08.js
@@ -1,32 +1,15 @@
-function isNotANumber(value){
-  return typeof value !== "number";
-}
-
-console.log(isNotANumber(33));      // false 
-console.log(isNotANumber("bob"));   // true
-console.log(isNotANumber('33'));    // true
-console.log(isNotANumber([1, 5]));  // true
-
-
-/*
-Expanded version of the function
-
-function isNotANumber(value){
-  if(typeof value === "number"){
-    return false;
-  } else 
-    return true;
-}
-
-------------------------------------
-Solution from the book
+// Solution from the book
 
 function isNotANumber(value) {
   return value !== value;
 }
 
-explanation: 
-This works since NaN is the only 
-JS value that is not === to itself.
+console.log(isNotANumber(33));      // false 
+console.log(isNotANumber("bob"));   // false
+console.log(isNotANumber('33'));    // false
+console.log(isNotANumber([1, 5]));  // false
+console.log(isNotANumber(NaN));     // true
 
-*/
+// explanation: This works since NaN is the only JS value that is not === to itself.
+
+
